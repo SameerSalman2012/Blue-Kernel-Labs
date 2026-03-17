5e458b0bcb54d4fc67e75c61d840e59c7f35a71c
SameerSalman2012 <muhammadsameersalman@gmail.com>
2026-03-17 19:19:23 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,17 +1,11 @@
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
-  const key = process.env.GROQ_API_KEY;
-
-  if (!key) {
-    return res.status(500).json({ error: 'No API key found' });
-  }
-
   const response = await fetch('https://api.groq.com/openai/v1/chat/completions', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${key}`
+      'Authorization': 'Bearer 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'
     },
     body: JSON.stringify(req.body)
   });
